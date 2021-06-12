11938324b650deac9aafc1a073dbc08deafdc231
aguescribano87 <aguescribano87@gmail.com>
2021-06-12 19:41:54 -0300

    Header

---
+++ src/Components/Layout/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {Header} from "./Header/header"
+import {Footer} from "./Footer/footer"
+
+const Layout = ({children})=>{
+    return (
+        <>
+            <Header />
+            {children}
+            <Footer />
+
+        </>
+    )
+}
+
+export {Layout}
