5b7458dde2dcf29e54842b09ff6c2f9a883491ca
Agustin Escribano <35698909+aguescribano87@users.noreply.github.com>
2023-06-21 16:49:09 -0300

    prueba

--- src/Components/buyOk/buyOk.jsx
+++ src/Components/buyOk/buyOk.jsx
@@ -1,2 +1,2 @@
 
-export const BuyOk = ({ ordenId, nombre }) => <div>{nombre} gracias por su COMPRA su código de referencia es: {ordenId} </div>
+export const BuyOk = ({ ordenId, nombre }) => <div>{nombre} gracias por su COMPRA su código de referencia Es: {ordenId} </div>
